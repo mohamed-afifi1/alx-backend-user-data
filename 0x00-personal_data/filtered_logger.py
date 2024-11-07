544ece3f1610050fb85ef77e71048c004d2cc5af
root <mohammed2004koki@gmail.com>
2024-11-08 01:24:32 +0200

    test

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,12 @@
 """personal data"""
 
 from typing import List
-import re
 import logging
+import re
+from mysql.connector import connection
+from os import environ
 
+PII_FIELDS = ('name', 'email', 'password', 'ssn', 'phone')
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
@@ -15,6 +18,32 @@ def filter_datum(fields: List[str], redaction: str,
                       field + "=" + redaction + separator, temp)
     return temp
 
+def get_logger() -> logging.Logger:
+    """ Returns logger obj  """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+    return logger
+
+
+def get_db() -> connection.MySQLConnection:
+    """
+    Connect to mysql server with environmental vars
+    """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    db_host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+    connector = connection.MySQLConnection(
+        user=username,
+        password=password,
+        host=db_host,
+        database=db_name)
+    return connector
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
